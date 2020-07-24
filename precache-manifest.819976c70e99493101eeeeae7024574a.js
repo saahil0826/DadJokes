7bad079dab1a73f08446b56d2805cfdd02942174
saahil0826 <snawalkha0826@gmail.com>
2020-07-24 22:21:22 +0530

    Updates

---
+++ precache-manifest.819976c70e99493101eeeeae7024574a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0181583707847fd74f557940519948",
+    "url": "/DadJokes/index.html"
+  },
+  {
+    "revision": "bf7c142cd2f29af8c69f",
+    "url": "/DadJokes/static/css/main.c8ff0cfc.chunk.css"
+  },
+  {
+    "revision": "314c3d9bbeb529c73873",
+    "url": "/DadJokes/static/js/2.8cf5881f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DadJokes/static/js/2.8cf5881f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7c142cd2f29af8c69f",
+    "url": "/DadJokes/static/js/main.761f9e7c.chunk.js"
+  },
+  {
+    "revision": "43c185b580579270cd43",
+    "url": "/DadJokes/static/js/runtime-main.27017b71.js"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/DadJokes/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/DadJokes/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "46e967b815ca51f5f3d477f178662f51",
+    "url": "/DadJokes/static/media/GothamBoldRegular.46e967b8.ttf"
+  }
+]);
